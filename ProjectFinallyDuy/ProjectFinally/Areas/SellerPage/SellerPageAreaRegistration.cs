68254a3968622120e2d4af72dffe1f26569068ce
Nguyễn Khánh Duy <khanhduy1999rachkien@gmail.com>
2019-10-15 00:59:35 +0700

    Update 15/10

---
+++ ProjectFinallyDuy/ProjectFinally/Areas/SellerPage/SellerPageAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace ProjectFinally.Areas.SellerPage
+{
+    public class SellerPageAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "SellerPage";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "SellerPage_default",
+                "SellerPage/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
